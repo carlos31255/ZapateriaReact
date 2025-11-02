a71c78bdfec50728cab3dba9a61de390eaa0a92f
carlos31255 <car.hidalgoa@duocuc.cl>
2025-11-01 21:06:54 -0300

    Centralizado estilos css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-// ============================================
 // COMPONENTE PRINCIPAL DE LA APLICACIÓN
-// ============================================
 
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import { AppRoutes } from './routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 
 function App() {
   return (
